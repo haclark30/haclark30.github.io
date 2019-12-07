d3b2304b4ca3b84ffd352bd80ff12d21755462d2
haclark <haclark30@gmail.com>
2019-12-07 10:15:28 -0600

    update gitignore, added .net app

---
+++ ResumeMdToHtml/ResumeMdToHtml/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Markdig;
+
+namespace ResumeMdToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\..\..\resume.md";
+            var markdown = File.ReadAllText(path);
+            var html = Parse(markdown);
+            File.WriteAllText(@"..\..\..\..\..\resume.html", html, Encoding.UTF8);
+            //Console.WriteLine(html);
+        }
+
+        public static string Parse(string markdown)
+        {
+            var pipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .Build();
+            return Markdown.ToHtml(markdown, pipeline);
+        }
+    }
+}
